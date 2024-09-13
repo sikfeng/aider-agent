e3c55de8d7dd01251204d8ce6a81191eae4043cb
Sik Feng Cheong <sikfeng@gmail.com>
2024-09-14 01:36:53 +0800

    lots of pep8 formatting test dir for tests

--- tests/test_stream.py
+++ tests/test_stream.py
@@ -56,7 +56,7 @@ async def test():
 
     for subtask in subtasks:
         logger.info("Running subtask: %s", subtask)
-        #await test_websocket_endpoint(uri, "run_subtask", {"subtask": subtask})
+        await test_websocket_endpoint(uri, "run_subtask", {"subtask": subtask})
 
     await test_websocket_endpoint(uri, "undo_last_subtask")
 
