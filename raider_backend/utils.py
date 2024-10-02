f1d3eb242d20a7909409f6ab8585b1b2c83ff78c
Sik Feng Cheong <sikfeng@gmail.com>
2024-10-02 16:27:50 +0800

    fix logfile name and store in raider_backend directory

--- raider_backend/utils.py
+++ raider_backend/utils.py
@@ -28,7 +28,8 @@ def get_tmp_file(name: str) -> str:
     :param name: The name of the temporary file.
     :return: The temporary file path.
     """
-    return str(Path(tempfile.gettempdir()) / f"name_{datetime.datetime.now().strftime('%Y-%m-%d %Hh%Mm%Ss')}.log")
+    (Path(tempfile.gettempdir()) / "raider_backend").mkdir(parents=True, exist_ok=True)
+    return str(Path(tempfile.gettempdir()) / "raider_backend" / f"{name}_{datetime.datetime.now().strftime('%Y-%m-%d %Hh%Mm%Ss')}.log")
 
 
 def get_absolute_path(path: str) -> str:
