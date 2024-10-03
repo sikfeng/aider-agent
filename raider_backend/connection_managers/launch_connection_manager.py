b1f223aa24cbd3d4b1218127bf4d26e7a5c31993
Prannaya <prannayagupta@gmail.com>
2024-10-03 19:55:59 +0800

    feat: move web raider into launch connection manager

--- raider_backend/connection_managers/launch_connection_manager.py
+++ raider_backend/connection_managers/launch_connection_manager.py
@@ -1,5 +1,5 @@
 from typing import Any, Dict
-
+import json
 from fastapi import WebSocket
 
 from raider_backend.handlers.agent_manager_handler import AgentManagerHandler, InitAgentManagerError
@@ -23,15 +23,33 @@ class LaunchConnectionManager(BaseConnectionManager):
             The expected format of the `data` parameter is a dictionary
             with at least three keys: `main_repo_dir`, `method` and `params`.
         """
-        main_repo_dir = data.get("main_repo_dir")
+        
         method = data.get("method")
         params = data.get("params", {})
-
-        try:
-            async for response in self.agent_manager_handler.handle_message(
-                session_id=session_id, main_repo_dir=main_repo_dir, method=method, params=params):
-                await self.send_message(websocket, response, session_id)
-        except InitAgentManagerError as e:
-            await self.send_message(websocket, {"error": str(e)}, session_id)
-        finally:
-            await self.send_message(websocket, BaseConnectionManager.END_OF_MESSAGE_RESPONSE, session_id)
+        main_repo_dir = data.get("main_repo_dir")
+        
+        if method == "query":
+            try:
+                import web_raider
+            except ModuleNotFoundError as e:
+                self.logger.error("Web Raider package not found")
+                raise e
+            
+            response = json.dumps([
+                dict(
+                    type=item.get("type", "").lower(),
+                    name=item.get("name", ""),
+                    url=item.get("url", "")
+                ) for item in json.loads(web_raider.pipeline_main(**params))
+            ])
+            await self.send_message(websocket, {"result": response}, session_id)
+        
+        else:
+            try:
+                async for response in self.agent_manager_handler.handle_message(
+                    session_id=session_id, main_repo_dir=main_repo_dir, method=method, params=params):
+                    await self.send_message(websocket, response, session_id)
+            except InitAgentManagerError as e:
+                await self.send_message(websocket, {"error": str(e)}, session_id)
+        
+        await self.send_message(websocket, BaseConnectionManager.END_OF_MESSAGE_RESPONSE, session_id)
