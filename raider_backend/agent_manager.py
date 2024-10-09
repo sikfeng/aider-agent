2ef3b8be0cf936d1a4256d7ec9cfa6b61cf6d2e0
Sik Feng Cheong <sikfeng@gmail.com>
2024-10-09 22:35:23 +0800

    remove single quotes surrounding main_repo_agent if exists

--- raider_backend/agent_manager.py
+++ raider_backend/agent_manager.py
@@ -281,6 +281,10 @@ def main():
     # Parse command-line arguments
     args = parser.parse_args()
 
+    # some shells pass the single quotes directly to the argument, need to remove them
+    if args.main_repo_dir[0] == "'" and args.main_repo_dir[-1] == "'":
+        args.main_repo_dir = args.main_repo_dir[1:-1]
+
     # Configure logging
     LOG_CONFIG['handlers']['fileHandler']['filename'] = utils.get_absolute_path(args.logfile)
 
